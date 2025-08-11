3309be138c0265ba84f60371ed3c749132182803
CodeX60 <151678852+AzizSaddique@users.noreply.github.com>
2025-08-11 10:04:02 -0700

    first commit

---
+++ basic.py
@@ -0,0 +1,49 @@
+# print("Hello World!")
+# n=input('\tawhat is your name?\n')
+# print('my name is',n)
+# a=int (input('Enter first number?'))
+# b=int(input('Enter second number?'))
+# if a==b:
+#     print(a+b)
+# else:
+#     print('a is not equal to b')
+# 
+# import datetime
+# day_dalta=datetime.timedelta(days=1)
+# start_date=datetime.date.today()
+# end_date=start_date + 10*day_dalta
+# for i in range((end_date-start_date).days):
+   
+#     print(start_date + i*day_dalta)
+
+# a='python is fun'
+# b='python is'
+
+# c=5
+# d=3
+# print(a is b)
+# print(c is d)
+
+# ~~~~~~~~~~~~~~~~~~~Loop Chapter~~~~~~~~~~~~~~~~~~~~
+
+# def func(params):
+#     for value in params:
+#         print('got the value{}'.format(value))
+#         if value==1:
+#             return
+#         print('Still looping')
+#     return 'could not find 1 in the list'
+# func([5,3,2,8,9])
+
+# ~~~~~~~~~~~~~~~~~~~ Importing module ~~~~~~~~~~~~~~~~~~~~
+
+
+# import random
+# print(random.randint(1, 10))
+
+# from math import sin,cos,tan,pi
+# print(sin(1))
+
+# from mymodule import *
+
+
---
+++ project.py
@@ -0,0 +1,38 @@
+# import libraries
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.graph_objects as go
+import plotly.express as px
+import datetime as dt
+from datetime import timedelta
+from statsmodels.tsa.seasonal import seasonal_decompose
+import statsmodels.api as sm
+
+# tilte
+app_title = "Stock Price Analysis App"
+st.title(app_title)
+
+st.subheader("Analyze stock prices and visualize trends")
+# sidebar
+
+st.image('https://images.timesnownews.com/thumb/msid-151515654,thumbsize-36610,width-1280,height-720,resizemode-75/151515654.jpg')
+st.sidebar.header("User Input")
+start_date=st.sidebar.date_input('Start date', value=dt.date(2020, 1, 1))
+end_date=st.sidebar.date_input('End date', value=dt.date(2020, 12, 31))
+
+# add ticker in symbol
+ticker_list=("AAPL , MSFT, GOOGL, AMZN, TSLA, FB, NFLX, NVDA, BRK-A, V, JNJ, WMT, PG, UNH, DIS, MA, HD, PYPL, VZ, T, KO")
+ticker=st.sidebar.selectbox('Select company...',ticker_list.split(', '))
+# fetch data from user input using yfinance library
+data=yf.download(ticker,start=start_date,end=end_date)
+
+
+# add date as an column in dataframe
+data.insert(0, 'Date', data.index)
+data.reset_index(drop=True, inplace=True)
+st.write('Data from', start_date,'to',end_date)
+st.write(data)
